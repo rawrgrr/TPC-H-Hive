6cf29dc72ec2cf806b05241a549030849d499208
Roger Tu <rtu@dropbox.com>
2016-06-15 14:56:09 -0700

    update run_tpch_benchmark.sh to take a description

--- TPC-H_on_Hive/run_tpch_benchmark.sh
+++ TPC-H_on_Hive/run_tpch_benchmark.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# ./tpch_benchmark.sh 'serial'|'parallel' num_trials
-./tpch_benchmark.sh "$1" "$2" | tee "tpch_100_$1_log_$(date +%FT%TZ.log)"
+# ./tpch_benchmark.sh 'serial'|'parallel' num_trials description
+./tpch_benchmark.sh "$1" "$2" | tee "tpch_100_$1_$3_log_$(date +%FT%TZ.log)"
